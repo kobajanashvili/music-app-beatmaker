8eb4990dfa92e66565b9a2a8031a8d390ddb0820
kobajanashvili <kobajanashvili@gmail.com>
2019-04-27 16:07:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ window.addEventListener('load', () => {
 
 
     // Lets get going with sound here !!!
-    
+    pads.forEach(pad => {
+        pad.addEventListener('click', function(){
+            sounds[3];
+        });
+    });
 });
+
+console.log(sounds);
