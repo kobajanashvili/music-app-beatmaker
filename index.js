a73ffe3ad93c8e6c4243d5f966c4fb1c2fa73822
kobajanashvili <kobajanashvili@gmail.com>
2019-04-27 16:25:18 +0300

    add animation

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 window.addEventListener('load', () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector('.visual');
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "#c460d3",
+        "#d3d160",
+        "#ea12f1",
+        "#15a7cc"
+    ];
 
 
     // Lets get going with sound here !!!
@@ -8,7 +17,17 @@ window.addEventListener('load', () => {
         pad.addEventListener('click', function(){
             sounds[index].currentTime = 0;
             sounds[index].play();
+
+            createBubbles(index);
         });
     });
+
+    // create a function that make colors bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease";
+    };
 });
 
