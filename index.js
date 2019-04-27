c7f8d430d59bd4c9ec9141a2c3ab32962e781925
kobajanashvili <kobajanashvili@gmail.com>
2019-04-27 16:05:12 +0300

    add js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
 
+
+    // Lets get going with sound here !!!
+    
 });
